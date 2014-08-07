ccfe4df71a7f87b973b52792df3eca3a6baadfeb
Brock Wilcox <awwaiid@thelackthereof.org>
2014-08-07 00:31:54 -0400

    Document and implement method #2

--- lib/pry-timetravel.rb
+++ lib/pry-timetravel.rb
@@ -18,8 +18,19 @@ class PryTimetravel
       if child_pid
         dlog("PARENT #{parent_pid}: I have a child pid #{child_pid}")
 
-        @previous_pid ||= []
-        @previous_pid.push child_pid
+        # Method 1: Child suspends themselves, parent adds them to list
+        #  @previous_pid ||= []
+        #  @previous_pid.push child_pid
+
+        # Method 2: Parent does a global-blocking waitall
+        # when the child exits, we can continue
+        # Problem: What if the child exits in a bad way? Check out '$?'
+        Process.waitpid(child_pid)
+        dlog("ME #{$$}: Previous exit: #{$?.inspect}")
+        if $?.exitstatus != 42
+          # Since this wasn't a timetravel return, we must unravel this world
+          Kernel.exit! $?.exitstatus
+        end
 
         # The parent universe freezes
         #  dlog("PARENT #{$$}: I am suspending. My child is #{parent_pid}")
@@ -29,12 +40,19 @@ class PryTimetravel
         #  dlog("PARENT #{$$}: resumed!")
       else
         child_pid = $$
-
         dlog("CHILD: #{child_pid}: I have a parent pid #{parent_pid}")
 
-        Process.kill 'SIGSTOP', child_pid
+        # Method 1: Child suspends themselves, parent adds them to list
+        # The child is eventually resumed
+        #  Process.kill 'SIGSTOP', child_pid
+        #  dlog("CHILD #{child_pid}: resumed!")
 
-        dlog("CHILD #{child_pid}: resumed!")
+        # Method 2: Parent does a global-blocking waitall
+        # Child doesn't need to keep track of parent at all?
+        # Child will just exit! when it is done
+        @previous_pid ||= []
+        @previous_pid.push parent_pid
+
       end
     end
 
@@ -43,15 +61,24 @@ class PryTimetravel
       dlog("ME #{$$}: previous_pid = #{ @previous_pid }");
       if @previous_pid && ! @previous_pid.empty?
         previous_pid = @previous_pid.pop
-
         dlog("ME #{$$}: I found a previous pid #{previous_pid}! TIME TRAVEL TIME")
-        Process.kill 'SIGCONT', previous_pid
-        dlog("ME #{$$}: I resumed pid #{previous_pid}... now time to wait")
-        #  Process.waitpid(previous_pid)
-        Process.waitall
-        dlog("ME #{$$}: If you meet your previous self, kill yourself.")
+
+        # Method 1: Awaken the child and let them take over
+        # Main parent can't exit or shell will get upset, so wait for all children
+        # Once all children are done, kill ourself
+        #  Process.kill 'SIGCONT', previous_pid
+        #  dlog("ME #{$$}: I resumed pid #{previous_pid}... now time to wait")
+        #  #  Process.waitpid(previous_pid)
+        #  Process.waitall
+        #  dlog("ME #{$$}: If you meet your previous self, kill yourself.")
+        #  #  Process.kill 'SIGKILL', $$
+        #  Kernel.exit!
+
+        # Method 2: Kill ourself and let the parent take over
+        # The parent was just doing a waitpid, so it is ready
         #  Process.kill 'SIGKILL', $$
-        Kernel.exit!
+        Process.exit! 42
+
       end
       dlog("ME #{$$}: I was unable to time travel. Maybe it is a myth.");
     end
