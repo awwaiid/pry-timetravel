26fc3bef83648ba8d56dff2cdaadaf3b8b41d022
Brock Wilcox <awwaiid@thelackthereof.org>
2014-10-19 21:18:37 -0400

    Support multiple snapshots and better signaling

--- lib/pry-timetravel.rb
+++ lib/pry-timetravel.rb
@@ -7,81 +7,110 @@ class PryTimetravel
   class << self
 
     def dlog(msg)
-      #  File.open("meta.log", 'a') do |file|
-        #  file.puts(msg)
-      #  end
+      if ENV["TIMETRAVEL_DEBUG"]
+        File.open("meta.log", 'a') do |file|
+          file.puts("#{Time.now} [#{$$}] #{msg}")
+        end
+      end
     end
 
-    def checkpoint
+    at_exit do
+      PryTimetravel.dlog "at_exit"
+      if $root_parent && $$ != $root_parent
+        PryTimetravel.dlog "Sending SIGUSR1 up to #{$root_parent}"
+        Process.kill 'SIGUSR1', $root_parent
+      end
+    end
+
+    def enter_suspended_animation
+      old_sigcont_handler = Signal.trap('CONT') do
+        dlog("Got a SIGCONT")
+      end
+
+      old_sigexit_handler = Signal.trap('EXIT') do
+        dlog("got EXIT")
+        Kernel.exit!
+      end
+
+      dlog("Stopping myself")
+      Process.kill 'SIGSTOP', $$
+      dlog("Back from SIGSTOP!")
+
+      dlog("Returning to old SIGCONT")
+      Signal.trap('CONT', old_sigcont_handler)
+      dlog("Returning to old SIGEXIT")
+      Signal.trap('EXIT', old_sigexit_handler)
+    end
+
+    def snapshot
+
+      # We need a root-parent to keep the shell happy
+      if ! $root_parent
+        $root_parent = $$
+        child_pid = fork
+        if child_pid
+          Signal.trap('INT') do
+            dlog("root-parent got INT")
+          end
+          Signal.trap('USR1') do
+            dlog("root-parent got USR1")
+            Kernel.exit!
+          end
+          dlog "Root parent waiting on #{child_pid}"
+          Process.waitpid child_pid
+          dlog "Root parent exiting!"
+          Kernel.exit!
+        end
+      end
+
+      $timetravel_root ||= $$
+
       parent_pid = $$
       child_pid = fork
       if child_pid
-        dlog("PARENT #{parent_pid}: I have a child pid #{child_pid}")
+        dlog("I am parent #{parent_pid}: I have a child pid #{child_pid}")
 
-        # Method 1: Child suspends themselves, parent adds them to list
-        #  @previous_pid ||= []
-        #  @previous_pid.push child_pid
+        # Method 3: Child suspends themselves, parent adds them to list
+        @previous_pid ||= []
+        @previous_pid.push child_pid
 
-        # Method 2: Parent does a global-blocking waitall
-        # when the child exits, we can continue
-        # Problem: What if the child exits in a bad way? Check out '$?'
-        Process.waitpid(child_pid)
-        dlog("ME #{$$}: Previous exit: #{$?.inspect}")
-        if $?.exitstatus != 42
-          # Since this wasn't a timetravel return, we must unravel this world
-          Kernel.exit! $?.exitstatus
-        end
+        # Perform operation
+        yield
 
-        # The parent universe freezes
-        #  dlog("PARENT #{$$}: I am suspending. My child is #{parent_pid}")
-        #  dlog("PARENT #{$$}: suspending")
-        #  Process.setpgrp
-        #  Process.setsid
-        #  dlog("PARENT #{$$}: resumed!")
       else
         child_pid = $$
-        dlog("CHILD: #{child_pid}: I have a parent pid #{parent_pid}")
-
-        # Method 1: Child suspends themselves, parent adds them to list
-        # The child is eventually resumed
-        #  Process.kill 'SIGSTOP', child_pid
-        #  dlog("CHILD #{child_pid}: resumed!")
-
-        # Method 2: Parent does a global-blocking waitall
-        # Child doesn't need to keep track of parent at all?
-        # Child will just exit! when it is done
-        @previous_pid ||= []
-        @previous_pid.push parent_pid
-
+        dlog("I am child #{child_pid}: I have a parent pid #{parent_pid}")
+        enter_suspended_animation
       end
     end
 
-    def go_back
-      dlog("ME #{$$}: Thinking about time travel...");
-      dlog("ME #{$$}: previous_pid = #{ @previous_pid }");
-      if @previous_pid && ! @previous_pid.empty?
-        previous_pid = @previous_pid.pop
-        dlog("ME #{$$}: I found a previous pid #{previous_pid}! TIME TRAVEL TIME")
-
-        # Method 1: Awaken the child and let them take over
-        # Main parent can't exit or shell will get upset, so wait for all children
-        # Once all children are done, kill ourself
-        #  Process.kill 'SIGCONT', previous_pid
-        #  dlog("ME #{$$}: I resumed pid #{previous_pid}... now time to wait")
-        #  #  Process.waitpid(previous_pid)
-        #  Process.waitall
-        #  dlog("ME #{$$}: If you meet your previous self, kill yourself.")
-        #  #  Process.kill 'SIGKILL', $$
-        #  Kernel.exit!
-
-        # Method 2: Kill ourself and let the parent take over
-        # The parent was just doing a waitpid, so it is ready
-        #  Process.kill 'SIGKILL', $$
-        Process.exit! 42
-
-      end
-      dlog("ME #{$$}: I was unable to time travel. Maybe it is a myth.");
+    def snapshot_list
+      @previous_pid && @previous_pid.join(" ")
     end
+
+    def restore_snapshot(target_pid = nil, count = nil)
+      dlog("Thinking about time travel...");
+
+      if target_pid.nil? && @previous_pid && ! @previous_pid.empty?
+        count = 1 if count == 0
+        target_pid = @previous_pid[-count]
+      else
+        target_pid = target_pid
+      end
+
+      if target_pid
+        dlog("ME #{$$}: I found a target pid #{target_pid}! TIME TRAVEL TIME")
+        Process.kill 'SIGCONT', target_pid
+        enter_suspended_animation
+      else
+        dlog("I was unable to time travel. Maybe it is a myth.");
+      end
+    end
+
+    def restore_root_snapshot
+      restore_snapshot($timetravel_root) if $timetravel_root
+    end
+
   end
 end
 
--- lib/pry-timetravel/commands.rb
+++ lib/pry-timetravel/commands.rb
@@ -1,49 +1,57 @@
 
-Pry::Commands.create_command "checkpoint", "Set a marker in the timeline" do
-  match 'checkpoint'
+Pry::Commands.create_command "snap", "Create a snapshot that you can later return to" do
+  match 'snap'
   group 'Timetravel'
-  #  description 'Snapshot the world so we can timetravel back here later'
   banner <<-'BANNER'
-    Usage: checkpoint
-           checkpoint --list
-           checkpoint --delete [INDEX]
+    Usage: snap [cmd]
+           snap --list
 
-    This will add a checkpoint which you can return to later.
+    This will add a snapshot which you can return to later.
+    
+    If you provide [cmd] then that command will also be run -- nice for "snap next" in conjunction with pry-byebug.
   BANNER
 
-  #  def options(opt)
-    #  opt.on :d, :delete,
-      #  "Delete the checkpoint with the given index. If no index is given delete them all",
+  def options(opt)
+    #  opt.on :d, :delete=,
+      #  "Delete the snapshot with the given index. If no index is given delete them all",
       #  :optional_argument => true, :as => Integer
-    #  opt.on :l, :list,
-      #  "Show all checkpoints"
-  #  end
+    opt.on :l, :list,
+      "Show a list of existing snapshots"
+  end
   def process
-    PryTimetravel.checkpoint
+    if opts.l?
+      output.puts PryTimetravel.snapshot_list
+    else
+      PryTimetravel.snapshot do
+        run(args.join(" ")) unless args.empty?
+      end
+    end
   end
 end
 
-Pry::Commands.create_command "timetravel", "Set a marker in the timeline" do
-  match 'timetravel'
+Pry::Commands.create_command "back", "Go back to the most recent snapshot" do
+  match 'back'
   group 'Timetravel'
-  #  description 'Snapshot the world so we can timetravel back here later'
   banner <<-'BANNER'
-    Usage: checkpoint
-           checkpoint --list
-           checkpoint --delete [INDEX]
+    Usage: back [count]
+           back --pid pid
+           back --home
 
-    This will add a checkpoint which you can return to later.
+    Go back to a previous snapshot.
   BANNER
 
-  #  def options(opt)
-    #  opt.on :d, :delete,
-      #  "Delete the checkpoint with the given index. If no index is given delete them all",
-      #  :optional_argument => true, :as => Integer
-    #  opt.on :l, :list,
-      #  "Show all checkpoints"
-  #  end
+  def options(opt)
+    opt.on :p, :pid=, "Jump (back) to a specific snapshot identified by [pid]",
+      :as => Integer
+    opt.on :home, "Jump to the end of the original execution sequence"
+  end
   def process
-    PryTimetravel.go_back
+    if opts.h?
+      PryTimetravel.restore_root_snapshot
+    else
+      count = args.first.to_i
+      PryTimetravel.restore_snapshot(opts[:p], count)
+    end
   end
 end
 
