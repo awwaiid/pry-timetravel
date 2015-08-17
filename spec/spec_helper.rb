da8c358bdf35e33282bee242d2b9e0c9d7b04b49
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-16 21:32:49 -0400

    Add some basic spec structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,25 @@
 require 'rspec'
-require 'rspec/autorun'
-
-require 'pry/test/helper'
-
 require './lib/pry-timetravel'
+
+#require 'pry/test/helper'
+
+# in case the tests call reset_defaults, ensure we reset them to
+# amended (test friendly) values
+# class << Pry
+#   alias_method :orig_reset_defaults, :reset_defaults
+#   def reset_defaults
+#     orig_reset_defaults
+
+#     Pry.config.color = false
+#     Pry.config.pager = false
+#     Pry.config.should_load_rc      = false
+#     Pry.config.should_load_local_rc= false
+#     Pry.config.should_load_plugins = false
+#     Pry.config.history.should_load = false
+#     Pry.config.history.should_save = false
+#     Pry.config.correct_indent      = false
+#     Pry.config.hooks               = Pry::Hooks.new
+#     Pry.config.collision_warning   = false
+#   end
+# end
+# Pry.reset_defaults
