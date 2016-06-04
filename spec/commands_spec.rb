65cfdd19f7e15b96e9e15a779c281e1bd38b6786
Brock Wilcox <awwaiid@thelackthereof.org>
2016-06-04 14:18:20 -0400

    Add another child-count test

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -12,6 +12,7 @@ describe "pry-timetravel" do
       expect(found).to be_truthy
     end
   end
+
   it "Exits with 0 snapshots cleanly" do
     saved_pid = nil
     PTY.spawn(pry_timetravel_cmd) do |reader, writer, cmd_pid|
@@ -20,12 +21,13 @@ describe "pry-timetravel" do
       found = reader.expect(/No snapshots/,1)
       expect(found).to be_truthy
       writer.puts("exit")
-      sleep 1 # Give time to exit?
+      sleep 0.1 # Give time to exit?
     end
 
     pid_list = `ps h -o pid,ppid -g #{saved_pid}`.split(/\n/)
     expect(pid_list.count).to be == 0
   end
+
   it "creates one snapshot" do
     PTY.spawn(pry_timetravel_cmd) do |reader, writer, cmd_pid|
       writer.puts("snap")
@@ -38,6 +40,32 @@ describe "pry-timetravel" do
       expect(pid2.to_i).to be > 0
 
       pid_list = `ps h -o pid,ppid -g #{cmd_pid}`.split(/\n/)
+      # 1: root
+      # 2: base snapshot
+      # 3: current running branch
+      expect(pid_list.count).to be == 3
+    end
+  end
+
+  it "creates second snapshot" do
+    PTY.spawn(pry_timetravel_cmd) do |reader, writer, cmd_pid|
+      writer.puts("snap")
+      writer.puts("snap")
+      writer.puts("snap --list")
+
+      all1, pid1 = reader.expect(/^(\d+) \(0\) <main> 1/,1)
+      all2, pid2 = reader.expect(/^  (\d+) \(1\) <main> 1/,1)
+      all3, pid3 = reader.expect(/^    (\d+) \(2\) <main> 1 \*\*\*/,1)
+
+      expect(pid1.to_i).to be > 0
+      expect(pid2.to_i).to be > 0
+      expect(pid3.to_i).to be > 0
+
+      pid_list = `ps h -o pid,ppid -g #{cmd_pid}`.split(/\n/)
+      # 1: root
+      # 2: base snapshot
+      # 2: second snapshot
+      # 3: current running branch
       expect(pid_list.count).to be == 4
     end
   end
